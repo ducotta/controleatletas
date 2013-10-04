4db35ee0f0602fbd93b6cce5b6169b09a1149a32
Eduardo L Cotta <ducotta@gmail.com>
2013-10-04 14:15:38 -0300

    copiei o diretorio para dar o push

--- ControleAtleta/src/controleatleta/ControleAtleta.java
+++
@@ -1,9 +0,0 @@
-package controleatleta;
-
-public class ControleAtleta {
-
-    public static void main(String[] args) {
-        CadastroBoxeador cadastro = new CadastroBoxeador();
-        cadastro.setVisible(true);
-    }
-}
